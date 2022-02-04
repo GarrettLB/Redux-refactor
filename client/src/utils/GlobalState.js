fac230fe8930a4460fd05e7998110807106439f9
GLBryan <garrettleebryan@gmail.com>
2022-02-04 13:18:56 -0500

    initial state

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -2,6 +2,6 @@ import React from "react";
 import store from './store'
 import { Provider } from 'react-redux';
 
-export default function StoreProvider ({ value = [], ...props }) {
+export default function StoreProvider (props) {
   return <Provider store={store} {...props} />;
 };
